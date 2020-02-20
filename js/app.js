b833cc64e6d08a2e3f22c1c5ba1d6bca0187a92d
thantmyat <thantmyat1234@gmail.com>
2020-02-20 11:29:55 +0630

    move some style to js

--- js/app.js
+++ js/app.js
@@ -24,5 +24,46 @@ $(document).ready(function() {
 
 	$('ul').on('click', '.delete', function() {
 		$(this).parent().remove();
-	})
+	});
+
+	var styles = [
+		{
+			name: '#container',
+			style : {
+				width:"400px",
+				margin:"100px auto 0"
+			}
+		},
+		{
+			name: "#txt",
+			style : {
+				width:"350px",
+				textIndent:"10px",
+				lineHeight:"30px",
+				border:"none",
+				outline:"none",
+				float:"left"
+			}
+		},
+		{
+			name: "#btn", 
+			style :{
+				width:"50px",
+				height:"30px",
+				border:"none",
+				outline:"none",
+				borderRadius:"0",
+				background:"#16A05D",
+				color:"#fff"
+			}
+		}
+
+	];
+
+
+	for(var i = 0; i < styles.length; i++) {
+		$(styles[i].name).css(styles[i].style);
+	}
+
+
 });
